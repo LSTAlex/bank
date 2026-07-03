7d96fffac956849f40c9ea186c79fc5ca2b2831d
LSTAlex <alex35-24@mail.ru>
2026-07-03 15:54:59 +0300

    fixed HomeAdminController import

--- src/main/java/com/example/bank/Controllers/HomeAdminController.java
+++ src/main/java/com/example/bank/Controllers/HomeAdminController.java
@@ -2,22 +2,11 @@ package com.example.bank.Controllers;
 
 import com.example.bank.Service.AccountService;
 import com.example.bank.Service.TimeService;
-import com.example.bank.dto.AdminAccountViewDto;
-import com.example.bank.mapper.AccountMapper;
-import com.example.bank.model.CreditUsersModel;
-import com.example.bank.model.DebitUsersModel;
-import com.example.bank.model.SavingsUsersModel;
-import com.example.bank.repository.CreditUsersRepository;
-import com.example.bank.repository.DebitUsersRepository;
-import com.example.bank.repository.SavingsUserRpository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -26,13 +15,11 @@ public class HomeAdminController {
 
     private final TimeService timeService;
     private final AccountService accountService;
-    private final AccountMapper accountMapper;
 
-    public HomeAdminController(DebitUsersRepository debitUsersRepository, CreditUsersRepository creditUsersRepository, SavingsUserRpository savingsUserRpository, TimeService timeService, AccountService accountService, AccountMapper accountMapper) {
+    public HomeAdminController(TimeService timeService, AccountService accountService) {
 
         this.timeService = timeService;
         this.accountService = accountService;
-        this.accountMapper = accountMapper;
     }
 
 
